2cba6722df57062e681a55d25806e9bc75b151e8
tashenoff <kail.dead.kd@gmail.com>
2020-12-24 18:35:26 +0600

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+
+    $('.btn-cta').click(function () {
+      $('.overlay').addClass('is-open');
+      return false;
+    });
+  
+    $('.close-btn').click(function () {
+      $('.overlay').removeClass('is-open');
+    });
+  
+  });
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./src/**/*.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      maxWidth: {
+        '1180': '1180px',
+      
+       }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
